f4edc7026b3dbd73dc66ae9a72fb1d98ca376e17
tegvlarivs <roman.vesely@me.com>
2017-09-30 15:10:15 +0200

    Add files via upload

---
+++ alternate/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    //Tabs
+
+    $(".stripe").mouseenter(function() {
+        $(this).stop().animate({
+            width: '100vw'
+        });
+        $(this).find(".content").stop().fadeIn().children().show();
+    });
+
+    $(".stripe").mouseleave(function() {
+        $(this).stop().animate({
+            width: '40px'
+        });
+        $(this).find(".content").stop().fadeOut();
+    });
+
+});
